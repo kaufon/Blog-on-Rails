f086404aed2cd500b4e2a47bd95b1b7a0144b984
kaufon <kauanfvale@gmail.com>
2024-05-09 18:41:41 -0300

    ✨scripts: ccreate all CRUD scripts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,49 @@
 class ArticlesController < ApplicationController
   def index
+    @article = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit,status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.comments.destroy_all
+    @article.destroy
+    redirect_to root_path
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title,:body)
+    end
+    def comment_params 
+      params.require(:comment).permit(:commenter, :body)
+    end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get '/articles', to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
 end
